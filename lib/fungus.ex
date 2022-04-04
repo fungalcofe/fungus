07dc3bfdaa5ee51727cb5e89eecf6f064e32ef09
Antonin Dupont <contact+git@antonindupont.fr>
2022-04-04 16:57:23 +0200

    Remove useless auto generated unit tests

--- lib/fungus.ex
+++ lib/fungus.ex
@@ -1,18 +1,3 @@
 defmodule Fungus do
-  @moduledoc """
-  Documentation for `Fungus`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Fungus.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc false
 end
--- test/fungus_test.exs
+++ test/fungus_test.exs
@@ -1,8 +1,4 @@
 defmodule FungusTest do
   use ExUnit.Case
   doctest Fungus
-
-  test "greets the world" do
-    assert Fungus.hello() == :world
-  end
 end
